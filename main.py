321a2f852a57652715413c1872b05adba1eab9af
Dmytro <bober200045@gmail.com>
2020-02-25 23:34:26 +0200

    kuku

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, escape, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    name = request.args.get("name", "World")
+    return f'Dmytro Bober TZ-82'
+
+@app.route('/first')
+def kuku():
+    a = "qwerty"
+    b = 123
+    c = None
+    return f'{a}.{b}.{c}'
+    
+if name == '__main__':
+  app.run('0.0.0.0')
